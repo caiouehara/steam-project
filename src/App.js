312ab6f4a89ea319d4e4fb59650aa119cfc7b499
Caio Uehara <caioueharamartins@gmail.com>
2020-09-16 16:04:29 -0300

    Add UI Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import CameraWrapper from './CameraWrapper';
+import { Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import CameraWrapper from './components/CameraWrapper';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CameraWrapper></CameraWrapper>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={({navigation}) => ({ 
+            title: 'Home Page',
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate('Camera')}
+                title="Info"
+                color="#00cc00"
+              />),
+          })}
+        />
+        <Stack.Screen name="Camera" component={CameraWrapper}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-});
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen(){
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+});
